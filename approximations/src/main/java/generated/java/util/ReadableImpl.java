889f4bd5cc6dd3b91957029d24ddd64859b538f7
IgorFilimonov <st087840@student.spbu.ru>
2024-08-14 14:16:59 +0300

    Added ReadableImpl

--- approximations/src/main/java/generated/java/util/ReadableImpl.java
+++ approximations/src/main/java/generated/java/util/ReadableImpl.java
@@ -1,7 +1,10 @@
 package generated.java.util;
 
+import org.jacodb.approximation.annotation.Approximate;
+
 import java.io.IOException;
 
+@Approximate(java.lang.Readable.class)
 public interface ReadableImpl {
     public int read(CharBufferImpl cb) throws IOException;
 }
